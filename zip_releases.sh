2c6ba9eb8a39891977f0a9be10e7497eff378c7e
wewe <benoit.arguel@protonmail.com>
2022-11-23 21:20:22 +0100

    feat: (wip) zip zip_releases script

--- zip_releases.sh
+++ zip_releases.sh
@@ -1,17 +1,33 @@
+#!/bin/bash
+# Ce script fait les 3 archives de release dans un dossier parent.
+# Attention, mettre à jour le numéro de version dans package.json
+# avant de le lancer
+
+# 0) version dans package.json
+version="1.1.0"
 
 # 1) npm run build
-# 2) delete js/**.map.js
-# 3) variable version récupéré dans package.json
-# 4) zip du dossier chrome, nommé bones_chrome_v${version}.zip
-# 4) zip du dossier firefox, nommé firefox_chrome_v${version}.zip
-# 6) zip du code pour l'audit firefox nommé bones_audit_v${version}.zip
-#    avec comme élément :
-#     - ./chrome
-#     - ./firefox
-#     - ./src
-#     - .env.js
-#     - .babelrc
-#     - package.json
-#     - README.md
-#     - webpack.config.js
+npm run build:firefox && npm run build:chrome
 
+# 2) delete js/**.map.js
+rm rf -f firefox/js/**.js.map chrome/js/**.js.map
+
+# 4) zip des deux archives de version chrome et firefox
+zip -r bones_chrome_v${version}.zip chrome/**
+zip -r bones_firefox_v${version}.zip firefox/**
+
+# 5) zip de l'archive pour l'audit firefox (seulement les fichiers necessaire)
+zip -r bones_audit_firefox_v${version}.zip firefox src .babelrc package.json README.md webpack.config.js
+
+# 6) suppression et création du dossier release
+rm -rf ../bones_release_v${version} 
+mkdir ../bones_release_v${version}
+
+# 7) déplacement des archives dans le dossier release
+mv bones_chrome_v${version}.zip ../bones_release_v${version}
+mv bones_firefox_v${version}.zip ../bones_release_v${version}
+mv bones_audit_firefox_v${version}.zip ../bones_release_v${version}
+
+# TODO version package.json
+# replace . by - dans version
+# ziper chrome et firefox à la racine
